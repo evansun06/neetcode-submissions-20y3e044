b26dd78ed28ac51ba27d73c0e1d1c8711333806b
Evan Sun <168130991+evansun06@users.noreply.github.com>
2026-03-31 10:19:57 -0700

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i, n in enumerate(nums):
+            x = target - n
+            if x in map:
+                return [map[x], i]
+            else:
+                map[n] = i
