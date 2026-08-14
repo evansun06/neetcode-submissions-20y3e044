2d37605bd010625676f5e6ec6c6228b4e09351ab
Evan Sun <168130991+evansun06@users.noreply.github.com>
2026-08-14 16:50:42 -0700

    Add: palindromic-substrings - submission-3

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+
+        def expand_odd(i) -> int:
+            x = 0
+            while (i + x < len(s)) and (i - x >= 0):
+                if s[i+x] == s[i-x]:
+                    x += 1
+                else:
+                    break
+
+            return x
+                    
+
+        def expand_even(left, right):
+            x = 0
+            while (right + x < len(s)) and (left - x >= 0):
+                if s[right + x] == s[left - x]:
+                    x += 1
+                else:
+                    break
+            return x
+
+
+        for i in range(len(s)):
+            
+            # odd mid
+            count += expand_odd(i)
+
+            count += expand_even(i, i + 1)
+        
+        return count
+
