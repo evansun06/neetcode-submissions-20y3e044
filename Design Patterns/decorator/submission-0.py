47195f565e8fc51391553f887ba404fd65791aaf
Evan Sun <168130991+evansun06@users.noreply.github.com>
2026-08-15 13:09:53 -0700

    Add: decorator - submission-0

---
+++ Design Patterns/decorator/submission-0.py
@@ -0,0 +1,43 @@
+class Coffee(ABC):
+    @abstractmethod
+    def getCost(self):
+        pass
+
+class SimpleCoffee(Coffee):
+    def getCost(self):
+        return 1.1
+
+class CoffeeDecorator(Coffee):
+    def __init__(self, decoratedCoffee):
+        self.decoratedCoffee = decoratedCoffee
+
+    def getCost(self):
+        return self.decoratedCoffee.getCost()
+
+class MilkDecorator(CoffeeDecorator):
+    # Implement the Milk decorator
+
+    def __init__(self, coffee: Coffee):
+        super().__init__(coffee)
+
+    def getCost(self):
+        return self.decoratedCoffee.getCost() + 0.5
+
+class SugarDecorator(CoffeeDecorator):
+    # Implement the Sugar decorator
+
+    def __init__(self, coffee: Coffee):
+        super().__init__(coffee)
+
+    def getCost(self):
+        return self.decoratedCoffee.getCost() + 0.2
+
+class CreamDecorator(CoffeeDecorator):
+    # Implement the Cream decorator
+
+    def __init__(self, coffee: Coffee):
+        super().__init__(coffee)
+
+    def getCost(self):
+        return self.decoratedCoffee.getCost() + 0.7
+
