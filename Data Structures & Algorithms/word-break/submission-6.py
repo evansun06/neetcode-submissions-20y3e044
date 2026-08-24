2c828c13df3de512a051ecc58cec2cd0240993f5
Evan Sun <168130991+evansun06@users.noreply.github.com>
2026-08-24 12:01:59 -0700

    Add: word-break - submission-6

---
+++ Data Structures & Algorithms/word-break/submission-6.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        memo = [None] * len(s)
+        wordDict = set(wordDict)
+
+        def dfs(n: int):
+
+            if n == len(s):
+                return True
+
+            if memo[n] is not None:
+                return memo[n]
+
+            res = False
+
+            for i in range(n + 1, len(s) + 1):
+                candidate = s[n:i]
+                if candidate in wordDict:
+                    res = res or dfs(i)
+                
+            memo[n] = res
+
+            return res
+
+        return dfs(0)
+
+    """
+    n:0
+
+    [None, None, None, None, None, None, None, None]
+
+    """
+            
+            
+
+
+        
