0a2b2dea25328a74fa4f9c092a91429c618a7600
Evan Sun <168130991+evansun06@users.noreply.github.com>
2026-08-25 10:57:34 -0700

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        """
+        Top - Down Memoization Approach
+        Subproblem/Recursion: Solve the min amount of coins needed from 0 - amount
+        Optimization: Exponential runtime if bruteforce recursion (redundant
+        calculations of min coins on same amounts. Cache to reduce.)
+        """
+        memo = [None] * (amount + 1)
+
+        def dfs(value: int):
+
+            if value == 0:
+                return 0
+            
+            if memo[value] is not None:
+                return memo[value]
+            
+            min_coins = float('inf')
+            for coin in coins:
+
+                if value - coin >= 0:
+                    min_coins = min(
+                        min_coins,
+                        dfs(value - coin)
+                    )
+
+            min_coins += 1
+            memo[value] = min_coins
+            return min_coins
+
+        result = dfs(amount)
+        return result if result != float('inf') else -1
+            
+
