fa2d710157549012cce3aa29359e1ac23f3e6b2a
Evan Sun <168130991+evansun06@users.noreply.github.com>
2026-09-12 23:10:30 -0700

    Add: unique-paths-ii - submission-1

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        """
+        let (i, j) -> int represent the number of unique paths to the end
+
+        recurse and memoize dfs
+
+        base case:
+        - memo is not None -> return memo
+        - col, row = end -> return 1
+        - out of bounds -> return 0
+        """
+
+        rows, cols = len(obstacleGrid), len(obstacleGrid[0])
+
+        memo = [[None] * cols for _ in range(rows)]
+
+        def dfs(r, c):
+            
+            if r > rows - 1 or c > cols - 1:
+                return 0
+            
+            if obstacleGrid[r][c] == 1:
+                return 0
+
+            if r == rows - 1 and c == cols - 1:
+                return 1
+
+
+            if memo[r][c] is not None:
+                return memo[r][c]
+
+            res =  dfs(r + 1, c) + dfs(r, c + 1)
+            memo[r][c] = res
+            return res
+        
+        return dfs(0,0)
